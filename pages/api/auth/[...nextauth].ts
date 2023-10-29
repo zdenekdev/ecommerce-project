d039ca72f7ce411371ba8550d21a2b1017ae85ba
zdenekdev <kucera.contact@gmail.com>
2023-10-29 16:40:27 +0100

    git install and configuration

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
+import Stripe from "stripe";
 
 const prisma = new PrismaClient();
 
@@ -13,4 +14,24 @@ export default NextAuth({
     }),
   ],
   adapter: PrismaAdapter(prisma),
+  events: {
+    createUser: async ({ user }) => {
+      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+        apiVersion: "2022-11-15",
+      });
+
+      // Create a stripe customer
+      if (user.name && user.email) {
+        const customer = await stripe.customers.create({
+          email: user.email,
+          name: user.name,
+        });
+        // Update our prisma user with the stripecustomerid
+        await prisma.user.update({
+          where: { id: user.id },
+          data: { stripeCustomerId: customer.id },
+        });
+      }
+    },
+  },
 });

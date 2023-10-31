975b7e3eb7f9da171f3c0d4a476f7e6302bd6bda
zdenekdev <kucera.contact@gmail.com>
2023-10-31 16:54:51 +0100

    fetch of product data from stripe

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,36 @@
 import Image from "next/image";
+import Stripe from "stripe";
+import Product from "./components/Product";
 
-export default function Home() {
+const getProducts = async () => {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+    apiVersion: "2022-11-15",
+  });
+  const products = await stripe.products.list();
+
+  const productWithPrices = await Promise.all(
+    products.data.map(async (product) => {
+      const prices = await stripe.prices.list({ product: product.id });
+      return {
+        id: product.id,
+        name: product.name,
+        price: prices.data[0].unit_amount,
+        image: product.images[0],
+        currency: prices.data[0].currency,
+      };
+    })
+  );
+  return productWithPrices;
+};
+
+export default async function Home() {
+  const products = await getProducts();
+  console.log(products);
   return (
     <main className="">
-      <h1>Hello</h1>
+      {products.map((product) => (
+        <Product {...product} />
+      ))}
     </main>
   );
 }

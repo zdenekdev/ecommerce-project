bc949950b61a7ea95b00fad5543a8e926674db0d
zdenekdev <kucera.contact@gmail.com>
2023-11-01 16:30:55 +0100

    created product page

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -1,14 +1,40 @@
 import Image from "next/image";
 import formatPrice from "@/util/PriceFormat";
 import { ProductType } from "@/types/ProductType";
+import Link from "next/link";
 
-function Product({ image, name, price }: ProductType) {
+function Product({
+  image,
+  name,
+  unit_amount,
+  id,
+  description,
+  metadata,
+}: ProductType) {
+  const { features } = metadata;
+
   return (
-    <div>
-      <Image src={image} alt={name} width={400} height={400} />
-      <h1>{name}</h1>
-      <h2>{price !== null ? formatPrice(price) : "N/A"}</h2>
-    </div>
+    <Link
+      href={{
+        pathname: `/product/${id}`,
+        query: { name, image, unit_amount, id, description, features },
+      }}>
+      <div className="text-gray-700">
+        <Image
+          src={image}
+          alt={name}
+          width={800}
+          height={800}
+          className="w-full h-96 object-cover rounded-lg"
+        />
+        <div className="font-medium py-2">
+          <h1>{name}</h1>
+          <h2 className="text-sm text-teal-700">
+            {unit_amount !== null ? formatPrice(unit_amount) : "N/A"}
+          </h2>
+        </div>
+      </div>
+    </Link>
   );
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,15 @@ const getProducts = async () => {
   const productWithPrices = await Promise.all(
     products.data.map(async (product) => {
       const prices = await stripe.prices.list({ product: product.id });
+      const features = product.metadata.features || "";
       return {
         id: product.id,
         name: product.name,
-        price: prices.data[0].unit_amount,
+        unit_amount: prices.data[0].unit_amount,
         image: product.images[0],
         currency: prices.data[0].currency,
+        description: product.description,
+        metadata: { features },
       };
     })
   );
@@ -25,9 +28,9 @@ const getProducts = async () => {
 
 export default async function Home() {
   const products = await getProducts();
-  console.log(products);
+  console.log("čau", products);
   return (
-    <main className="">
+    <main className="grid grid-cols-fluid gap-12">
       {products.map((product) => (
         <Product {...product} key={product.id} />
       ))}

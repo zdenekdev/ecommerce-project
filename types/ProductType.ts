d5497bbd02378b028beab94345a2a327cd1c959c
zdenekdev <kucera.contact@gmail.com>
2023-11-01 16:29:46 +0100

    edited ProductType and created SearchParamsTypes

--- types/ProductType.ts
+++ types/ProductType.ts
@@ -1,5 +1,13 @@
-export type ProductType {
-    name: string,
-    image: string,
-    price: number | null
-}
+export type ProductType = {
+  name: string;
+  image: string;
+  unit_amount: number | null;
+  quantity?: number | 1;
+  id: string;
+  description: string | null;
+  metadata: MetadataType;
+};
+
+type MetadataType = {
+  features: string;
+};

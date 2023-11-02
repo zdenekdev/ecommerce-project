0bd568815a5767d706dcb5d5fc0d0867b69a9508
zdenekdev <kucera.contact@gmail.com>
2023-11-02 17:01:22 +0100

    toggleCart added to store

--- store.ts
+++ store.ts
@@ -13,6 +13,7 @@ type CartItem = {
 type CartState = {
   isOpen: boolean;
   cart: CartItem[];
+  toggleCart: () => void;
 };
 
 export const useCartStore = create<CartState>()(
@@ -20,6 +21,7 @@ export const useCartStore = create<CartState>()(
     (set) => ({
       cart: [],
       isOpen: false,
+      toggleCart: () => set((state) => ({ isOpen: !state.isOpen })),
     }),
     { name: "cart-store" }
   )

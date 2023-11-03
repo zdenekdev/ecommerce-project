9d86ccd0f82d1f79527ece9bcc1dd738a453dc81
zdenekdev <kucera.contact@gmail.com>
2023-11-03 14:09:25 +0100

    addProduct functionality in store

--- store.ts
+++ store.ts
@@ -14,6 +14,7 @@ type CartState = {
   isOpen: boolean;
   cart: CartItem[];
   toggleCart: () => void;
+  addProduct: (item: CartItem) => void;
 };
 
 export const useCartStore = create<CartState>()(
@@ -22,6 +23,23 @@ export const useCartStore = create<CartState>()(
       cart: [],
       isOpen: false,
       toggleCart: () => set((state) => ({ isOpen: !state.isOpen })),
+      addProduct: (item) =>
+        set((state) => {
+          const existingItem = state.cart.find(
+            (cartItem) => cartItem.id === item.id
+          );
+          if (existingItem) {
+            const updatedCart = state.cart.map((cartItem) => {
+              if (cartItem.id === item.id) {
+                return { ...cartItem, quantity: cartItem.quantity + 1 };
+              }
+              return cartItem;
+            });
+            return { cart: updatedCart };
+          } else {
+            return { cart: [...state.cart, { ...item, quantity: 1 }] };
+          }
+        }),
     }),
     { name: "cart-store" }
   )

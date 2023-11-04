5c730f6d7eb6b3cd0bb3ed67ab01b1c3ba784613
zdenekdev <kucera.contact@gmail.com>
2023-11-04 16:43:41 +0100

    responsive cart

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -23,8 +23,12 @@ function Cart() {
       <motion.div
         layout
         onClick={(e) => e.stopPropagation()}
-        className="bg-white absolute right-0 top-0 w-1/4 h-screen p-12 overflow-y-scroll text-gray-700">
-        <h1>Here's your shopping list 🗒️</h1>
+        className="bg-white absolute right-0 top-0 h-screen p-12 overflow-y-scroll text-gray-700 w-full lg:w-2/5">
+        <button
+          onClick={() => cartStore.toggleCart()}
+          className="text-sm font-bold pb-12">
+          Back to store 🏃‍♂️
+        </button>
         {cartStore.cart.map((item) => (
           <motion.div layout key={item.id} className="flex py-4 gap-4">
             <Image
--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default async function RootLayout({
   const session = await getServerSession(authOptions);
   return (
     <html lang="en">
-      <body className={`${inter.className} mx-64 ${roboto.className}`}>
+      <body className={`${inter.className} mx-4 lg:mx-48 ${roboto.className}`}>
         <Hydrate>
           <Nav user={session?.user} expires={session?.expires as string} />
           {children}

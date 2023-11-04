fee03cd50d0de6c6c7e8ae3d3b085024a3356d48
zdenekdev <kucera.contact@gmail.com>
2023-11-04 13:22:21 +0100

    add and remove item button in the basket

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -2,6 +2,7 @@
 import { useCartStore } from "@/store";
 import formatPrice from "@/util/PriceFormat";
 import Image from "next/image";
+import { IoAddCircle, IoRemoveCircle } from "react-icons/io5";
 
 import React from "react";
 
@@ -28,7 +29,33 @@ function Cart() {
             />
             <div>
               <h2>{item.name}</h2>
-              <h2>Quantity: {item.quantity}</h2>
+              <div className="flex gap-2">
+                <h2>Quantity: {item.quantity}</h2>
+                <button
+                  onClick={() =>
+                    cartStore.removeProduct({
+                      id: item.id,
+                      image: item.image,
+                      name: item.name,
+                      unit_amount: item.unit_amount,
+                      quantity: item.quantity,
+                    })
+                  }>
+                  <IoRemoveCircle />
+                </button>
+                <button
+                  onClick={() =>
+                    cartStore.addProduct({
+                      id: item.id,
+                      image: item.image,
+                      name: item.name,
+                      unit_amount: item.unit_amount,
+                      quantity: item.quantity,
+                    })
+                  }>
+                  <IoAddCircle />
+                </button>
+              </div>
               <p className="text-sm">
                 {item.unit_amount && formatPrice(item.unit_amount)}
               </p>

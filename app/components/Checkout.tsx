b3e2646b989536ae817e0a68773f491906a681c1
zdenekdev <kucera.contact@gmail.com>
2023-11-05 13:37:24 +0100

    payment-intent created

---
+++ app/components/Checkout.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { loadStripe, StripeElementsOptions } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import { useCartStore } from "@/store";
+import { useState, useEffect } from "react";
+
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+);
+
+export default function Checkout() {
+  const cartStore = useCartStore();
+  const [clientSecret, setClientSecret] = useState("");
+
+  useEffect(() => {
+    // Create a paymentIntent as soon as page loads up
+    fetch("/api/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        items: cartStore.cart,
+        payment_intent_id: cartStore.paymentIntent,
+      }),
+    });
+  }, []);
+
+  return <h1>ahoj</h1>;
+}
